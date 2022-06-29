2789d8a62f39d9c465c3443c1c08f2a703e98076
Ashrith Jain <ashrithjain3524@gmail.com>
2022-06-29 16:53:18 +0530

    Commiting  App initialisation

---
+++ New York Times/New York Times/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  New York Times
+//
+//  Created by Ashrith Jain on 29/06/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
