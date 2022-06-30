6c0ebb4bbfdd800f97752808daf38d07d385662f
Ashrith Jain <ashrithjain3524@gmail.com>
2022-06-30 16:32:26 +0530

    Comiting ImageCache

--- New York Times/New York Times/Utilities/Extensions/UIImageView+Extension.swift
+++ New York Times/New York Times/Utilities/Extensions/UIImageView+Extension.swift
@@ -19,7 +19,8 @@ extension UIImageView{
 }
 
 extension UIImageView {
-    func downloaded(from url: URL, contentMode mode: ContentMode = .scaleAspectFill) {
+    
+    func downloaded(from url: URL, contentMode mode: ContentMode = .scaleAspectFill,urlString:String = "") {
         contentMode = mode
         URLSession.shared.dataTask(with: url) { data, response, error in
             guard
@@ -30,11 +31,37 @@ extension UIImageView {
                 else { return }
             DispatchQueue.main.async() { [weak self] in
                 self?.image = image
+                ImageCache.getImageCache().set(forKey: urlString, image: image)
             }
         }.resume()
     }
+    
     func downloaded(from link: String, contentMode mode: ContentMode = .scaleAspectFill) {
         guard let url = URL(string: link) else { return }
-        downloaded(from: url, contentMode: mode)
+        if let image = ImageCache.getImageCache().get(forKey: link){
+            self.image = image
+        }else{
+            downloaded(from: url, contentMode: mode,urlString: link)
+        }
+        
+    }
+}
+
+
+class ImageCache {
+    private static var imageCache = ImageCache()
+    
+    var cache = NSCache<NSString, UIImage>()
+    
+    func get(forKey: String) -> UIImage? {
+        return cache.object(forKey: NSString(string: forKey))
+    }
+    
+    func set(forKey: String, image: UIImage) {
+        cache.setObject(image, forKey: NSString(string: forKey))
+    }
+    
+    static func getImageCache() -> ImageCache {
+        return imageCache
     }
 }
